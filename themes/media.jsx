64b8ad675d50c2e556d8f3e7b3ec718b5e19a9a8
Byeonghun Song <rokaf340@gmail.com>
2024-07-24 23:29:08 +0900

    Revert "Feat: 스타일 가이드 설정"

--- themes/media.jsx
+++
@@ -1,9 +0,0 @@
-export const sizes = {
-  tablet: '768px',
-  desktop: '1024px',
-};
-
-export const media = {
-  tablet: `(min-width: ${sizes.tablet})`,
-  desktop: `(min-width: ${sizes.desktop})`,
-};
